e836a87e79374eb26ffd79ecd943dbc461e9ca33
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2021-09-16 21:53:57 +0200

    Cleanup the app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,7 @@
 import React from "react";
-import logo from "../img/logo.svg";
 import "../css/App.css";
 import "normalize.css";
 
 export const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+    <h1>Apple Expose</h1>
 );
