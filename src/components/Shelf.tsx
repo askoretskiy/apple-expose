738caf4ecf213fbbc5624be5972b7f9f26a0c17c
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2021-09-17 18:39:32 +0200

    Add shelf with neo buttons

--- src/components/Shelf.tsx
+++ src/components/Shelf.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Data, Item } from "../types/data";
+
+import { Button } from "ui-neumorphism";
+import "ui-neumorphism/dist/index.css";
+
 import "../css/App.css";
 import ExternalLink from "../img/external-link.svg";
 
 export const Shelf = ({ data }: { data: Data }) => (
-  <ul className="shelf">
-    {data.items.map((item) => (
-      <ShelfItem item={item} key={item.id} />
-    ))}
-  </ul>
+  <>
+    <ul className="shelf">
+      {data.items.map((item) => (
+        <ShelfItem item={item} key={item.id} />
+      ))}
+    </ul>
+    <div className="shelf-new">
+      {data.items.map((item) => (
+        <ShelfItemNeo item={item} key={item.id} />
+      ))}
+    </div>
+  </>
 );
+
+const ShelfItemNeo = ({ item }: { item: Item }) => <Button>{item.name}</Button>;
 
 const ShelfItem = ({ item }: { item: Item }) => (
   <li>
