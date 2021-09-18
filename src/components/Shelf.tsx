692490d366dd5f56d8d763a39aba51f78c6b5717
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2021-09-18 23:05:34 +0200

    Use neo component

--- src/components/Shelf.tsx
+++ src/components/Shelf.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Data, Item } from "../types/data";
 
-import { Button } from "ui-neumorphism";
+import { Card } from "ui-neumorphism";
 import "ui-neumorphism/dist/index.css";
 
 import "../css/App.css";
@@ -9,32 +9,12 @@ import ExternalLink from "../img/external-link.svg";
 
 export const Shelf = ({ data }: { data: Data }) => (
   <>
-    <ul className="shelf">
+    <div className="shelf">
       {data.items.map((item) => (
         <ShelfItem item={item} key={item.id} />
-      ))}
-    </ul>
-    <div className="shelf-new">
-      {data.items.map((item) => (
-        <ShelfItemNeo item={item} key={item.id} />
       ))}
     </div>
   </>
 );
 
-const ShelfItemNeo = ({ item }: { item: Item }) => <Button>{item.name}</Button>;
-
-const ShelfItem = ({ item }: { item: Item }) => (
-  <li>
-    [{item.presentYear}] {item.name}{" "}
-    {!item.generation ? null : `(gen. ${item.generation}) `}
-    <a
-      href={item.specUrl}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="external"
-    >
-      <img src={ExternalLink} className="external-link" alt="Specification" />
-    </a>
-  </li>
-);
+const ShelfItem = ({ item }: { item: Item }) => <Card>{item.name}</Card>;
